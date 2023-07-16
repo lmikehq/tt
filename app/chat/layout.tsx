6e4daf966d97e452d9c3cc2cf008991e2ecf201c
Michael E Adebisi <adebisienads@gmail.com>
2023-07-16 20:38:42 +0100

    chat seo metadata
    Signed-off-by: Michael E Adebisi <adebisienads@gmail.com>

--- app/chat/layout.tsx
+++ app/chat/layout.tsx
@@ -5,12 +5,18 @@ interface layoutProps {
   children: React.ReactNode;
 }
 
+export const metadata = {
+  title: "Engage, Explore, Enlighten: AI Chat and Travel Guide",
+  description:
+    " Utilize our intelligent AI to learn about any country of your choice, engage with our dedicated customer support, or simply chat with yourself! Exploring the world has never been this interactive. Connect with us and soar on the wings of the eagle",
+};
+
+
 export default function ChatLayout({ children }: layoutProps) {
   return (
     <>
       <Navbar page="dashboard" />
       {children}
-      {/* <FooterSection /> */}
     </>
   );
 }
