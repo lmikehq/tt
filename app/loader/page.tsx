2bfb65b17122adbf0b04725a8fc13f262b75061b
adeyemi pelumi obaloluwa <pelumiadeyemi111@gmail.com>
2023-07-29 19:12:43 +0100

    Loader add to the project

---
+++ app/loader/page.tsx
@@ -0,0 +1,12 @@
+
+
+import LoaderLayout from '@organism/Loader/layout'
+import React from 'react'
+
+export default function loader() {
+  return (
+      <LoaderLayout>
+          <h3>J4</h3>
+          </LoaderLayout>
+  )
+}
---
+++ components/organisms/Loader/loader.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import React from "react";
+import styled, { keyframes } from "styled-components";
+import Logo from "@image/airplaneIcon.gif";
+import Image from "../../atoms/image";
+import { ttColors } from "theme/colors";
+import Text from "@atom/text";
+
+
+
+
+
+
+const LoaderWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 9999;
+  background: "fff";
+  flex-direction: column;
+`;
+
+
+
+
+
+interface LoaderProps {
+  logo: string;
+}
+
+const Loader: React.FC<LoaderProps> = ({ logo }) => {
+  return (
+    <LoaderWrapper>
+      <>
+      <Image src={Logo} alt="logo" width={150} height={150} />
+        <Text type="h3" text="THRILLERS TRAVELS" color={ttColors.primary} />
+      </>
+      
+    </LoaderWrapper>
+  );
+};
+export default Loader;
