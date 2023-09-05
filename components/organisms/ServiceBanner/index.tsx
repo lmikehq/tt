fa11ef8c3dcf66a542698f3f65c16062e4819545
adeyemi pelumi obaloluwa <pelumiadeyemi111@gmail.com>
2023-09-05 20:05:57 +0100

    password validation in signup page and the auth pages done

--- components/organisms/ServiceBanner/index.tsx
+++ components/organisms/ServiceBanner/index.tsx
@@ -4,12 +4,13 @@ import ServiceTabs from "@molecule/serviceTabs";
 import { styled } from "styled-components";
 
 const BannerWrapper = styled.div`
-  width: 80vw;
+  // width: 80vw;
   background: var(--default-color);
   padding: 1.5rem 2rem;
   border-radius: 1rem;
   margin-top: 6rem;
   box-shadow: 0px 3px 24px #00000014;
+
 
   @media (max-width: 900px) {
     // transform: translateY(-1.5rem);
