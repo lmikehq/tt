1b51796a3e9b2f3fb73a2551d5de5ce56ffd082c
adeyemi pelumi obaloluwa <pelumiadeyemi111@gmail.com>
2023-09-05 15:26:20 +0100

    Password Validation in Signup Page is done.

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -15,7 +15,6 @@ import bgImage2 from "@image/auth-bg2.png";
 import bgImage3 from "@image/auth-bg3.jpeg";
 import bgImage4 from "@image/auth-bg4.jpeg";
 import bgImage5 from "@image/auth-bg5.jpeg";
-// import bgImage6 from "@image/auth-bg6.jpeg";
 import { Grid } from "@atom/grid";
 import SideBtn from "@atom/sideBtn";
 import bgImage7 from "@image/auth-bg7.jpeg";
@@ -32,6 +31,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 import { ttColors } from "theme/colors";
+import { AiFillCheckCircle } from "react-icons/ai";
 
 const settings = {
   infinite: true,
@@ -42,6 +42,34 @@ const settings = {
 };
 
 function RegisterPage() {
+  const [selectedOption, setSelectedOption] = useState("length");
+  const validationOptions = [
+    { value: "length", label: "8 or more characters" },
+    { value: "uppercaseLowercase", label: "Uppercase & Lowercase" },
+    { value: "number", label: "At least one number" },
+    {
+      value: "specialCharacter",
+      label: "Have Numbers, and Special symbols (e.g., !, @, #, $)",
+    },
+  ];
+
+  function isPasswordValid(password: string, selectedOption: string) {
+    switch (selectedOption) {
+      case "length":
+        return password.length >= 8;
+      case "uppercaseLowercase":
+        return /[A-Z]/.test(password) && /[a-z]/.test(password);
+      case "number":
+        return /\d/.test(password);
+      case "specialCharacter":
+        return /[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]/.test(password);
+      default:
+        return false;
+    }
+  }
+
+  const [confirmPassword, setConfirmPassword] = useState("");
+
   const { isMobile } = useScreenResolution();
 
   const router = useRouter();
@@ -89,11 +117,11 @@ function RegisterPage() {
         ...submissionState,
         error: [
           {
-            constraints: "Password and confirm password do not match",
+            constraints: "Password do not match",
             property: "confirmPassword",
           },
           {
-            constraints: "Password and confirm password do not match",
+            constraints: "Password do not match",
             property: "password",
           },
         ],
@@ -231,11 +259,11 @@ function RegisterPage() {
                 onClick={() => router.push("/")}
                 style={{ cursor: "pointer" }}
               />
-              <SideBtn
-                title="Already have an account?"
-                linkUrl="/auth/login"
-                linkText="Login"
-              />
+                <SideBtn
+                  title="Already have an account?"
+                  linkUrl="/auth/login"
+                  linkText="Login"
+                />
             </Flex>
             <Text
               type="h1"
@@ -377,7 +405,7 @@ function RegisterPage() {
                         : ""
                     }
                     height="3rem"
-                    type="tel"
+                    type="number"
                     value={registerData.phoneNumber}
                   />
                   {checkIfFieldHasError("phoneNumber") && (
@@ -389,11 +417,12 @@ function RegisterPage() {
                   )}
                 </Section>
               </Flex>
+
               <Section>
                 <Text
                   type="p"
                   text="Password"
-                  margin={isMobile ? ".7rem  0 .2rem" : "1rem 0 .5rem"}
+                  margin={isMobile ? ".7rem 0 .2rem" : "1rem 0 .5rem"}
                 />
                 <Input
                   placeholder="Enter your password"
@@ -410,36 +439,71 @@ function RegisterPage() {
                   height="3rem"
                   value={registerData.password}
                 />
-                {checkIfFieldHasError("password") && (
+
+                <Section margin="1rem 0px 0px">
                   <Text
-                    type="p"
-                    text={checkIfFieldHasError("password") || ""}
-                    color="#FF8682"
+                    type="h1"
+                    text="Your Password must have the following."
+                    size={16}
+                    weight={500}
+                    styles={{
+                      margin: "0px 0px .9rem 0px",
+                      lineHeight: "1.5rem",
+                    }}
                   />
-                )}
+                  {validationOptions.map((option) => (
+                    <div
+                      key={option.value}
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                        marginBottom: "0.5rem",
+                        fontSize: "16px",
+                        fontWeight: 400,
+                        color: isPasswordValid(
+                          registerData.password,
+                          option.value
+                        )
+                          ? "#000000"
+                          : "#000000",
+                      }}
+                    >
+                      <AiFillCheckCircle
+                        size="1.5rem"
+                        style={{
+                          color: isPasswordValid(
+                            registerData.password,
+                            option.value
+                          )
+                            ? "#7BBBD6"
+                            : "#B6B6B6",
+                        }}
+                      />
+                      <span style={{ marginLeft: "0.5rem" }}>
+                        {option.label}
+                      </span>
+                    </div>
+                  ))}
+                </Section>
               </Section>
+
               <Section>
                 <Text
                   type="p"
                   text="Confirm Password"
-                  margin={isMobile ? ".7rem  0 .2rem" : "1rem 0 .5rem"}
+                  margin={isMobile ? ".7rem 0 .2rem" : "1rem 0 .5rem"}
                 />
                 <Input
+                  placeholder="Confirm your password"
                   type="password"
-                  placeholder="Confirm Password"
-                  onChange={(e) =>
-                    setRegisterData({
-                      ...registerData,
-                      confirmPassword: e.target.value,
-                    })
-                  }
+                  onChange={(e) => setConfirmPassword(e.target.value)}
                   border={
                     checkIfFieldHasError("confirmPassword")
                       ? "1px solid #FF8682"
                       : ""
                   }
                   height="3rem"
-                  value={registerData.confirmPassword}
+                  value={confirmPassword}
                 />
                 {checkIfFieldHasError("confirmPassword") && (
                   <Text
@@ -456,7 +520,7 @@ function RegisterPage() {
                   margin={isMobile ? ".7rem  0 .2rem" : "1rem 0 .5rem"}
                 />
                 <Input
-                  placeholder="Enter your referral code"
+                  placeholder="n/a"
                   onChange={(e) =>
                     setRegisterData({
                       ...registerData,
@@ -508,7 +572,7 @@ function RegisterPage() {
                 </Flex>
               </Flex>
 
-              <div style={{ margin: "-1rem 0" }}>
+              {/* <div style={{ margin: "-1rem 0" }}>
                 {submissionState.error.length > 0 &&
                   submissionState.error.map((err: any, i: number) => (
                     <Text
@@ -519,7 +583,7 @@ function RegisterPage() {
                       key={i}
                     />
                   ))}
-              </div>
+              </div> */}
               <Button
                 width="100%"
                 margin="2rem 0 0"
@@ -546,7 +610,7 @@ function RegisterPage() {
                   display: isMobile ? "block" : "none",
                 }}
               >
-                Already have an account?{" "}
+                Already have an account?
                 <Link
                   href="/auth/login"
                   text="Login"
