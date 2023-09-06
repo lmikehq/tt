7669541107382329580ccadf46f019f0742b1ac5
adeyemi pelumi obaloluwa <pelumiadeyemi111@gmail.com>
2023-09-07 00:57:26 +0100

    Service banner err fixed finally

--- components/layouts/sectionLayout.tsx
+++ components/layouts/sectionLayout.tsx
@@ -3,7 +3,7 @@
 import styled from "styled-components";
 
 export default styled.section<{ margin?: string; padding?: string }>`
-  padding: ${(props) => props?.padding || "0 6.5rem"}; 
+  // padding: ${(props) => props?.padding || "0 6.5rem"}; 
   width: 80vw;
   max-width: 1600px;
   margin: ${(props) => props?.margin || "0 auto"};
