7a0aaf3e6afcb6ed83fed60effff0bf845c41851
adeyemi pelumi obaloluwa <pelumiadeyemi111@gmail.com>
2023-09-06 17:07:57 +0100

    Password validation and service banner err fixed

--- components/layouts/sectionLayout.tsx
+++ components/layouts/sectionLayout.tsx
@@ -3,8 +3,8 @@
 import styled from "styled-components";
 
 export default styled.section<{ margin?: string; padding?: string }>`
-  // padding: ${(props) => props?.padding || "0 6.5rem"};
-  width: 85vw;
+  padding: ${(props) => props?.padding || "0 6.5rem"}; 
+  width: 80vw;
   max-width: 1600px;
   margin: ${(props) => props?.margin || "0 auto"};
   position: relative;
