cda1f5b6c19ee41cebdee553c4f39db05ee783f3
Josh <joshue.dev@gmail.com>
2023-09-11 12:23:31 +0100

    Update .eslintrc.json

--- src/components/molecules/countryName.tsx
+++ src/components/molecules/countryName.tsx
@@ -4,7 +4,6 @@ import AllCountryHead from "@organism/AllCountry/allCountryHead";
 import React from "react";
 import { Qparams } from "types";
 import { unUrlString } from "src/lib/url";
-import SectionLayout from "@components/templates/SectionLayout";
 import CountryDetails from "./countryDetails";
 import { SAMPLE_COUNTRY_INFO } from "data/countryInfo";
 import { Grid } from "@components/templates/grid";
@@ -13,6 +12,8 @@ import { SAMPLE_COUNTRY_REQUIREMENT } from "data/countryRequirement";
 import HelpfulOrNot from "@molecule/helpfulOrNot";
 import SectionTitle from "src/components/molecules/sectionTitle";
 import { useScreenResolution } from "hook/useScreenResolution";
+import SectionLayout from "@components/templates/SectionLayout";
+
 
 interface pageProps {
   params: Qparams["params"];
--- src/components/templates/sectionLayout.tsx
+++ src/components/templates/sectionLayout.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import styled from "styled-components";
 
 export default styled.section<{ margin?: string; padding?: string }>`
