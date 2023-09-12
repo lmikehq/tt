4d830d5994e4167753741b8f61b9d76cff47b9e6
Abdulazeez Olalere <olalereazeez11@gmail.com>
2023-09-12 17:21:45 +0100

    FEAT:resolve import errors

--- src/components/templates/sectionLayout.tsx
+++ src/components/templates/sectionLayout.tsx
@@ -2,8 +2,8 @@
 
 import styled from "styled-components";
 
-export default styled.section<{ margin?: string; padding?: string }>`
-  // padding: ${(props) => props?.padding || "0 6.5rem"}; 
+const SectionLayout = styled.section<{ margin?: string; padding?: string }>`
+  // padding: ${(props) => props?.padding || "0 6.5rem"};
   width: 80vw;
   max-width: 1600px;
   margin: ${(props) => props?.margin || "0 auto"};
@@ -14,3 +14,5 @@ export default styled.section<{ margin?: string; padding?: string }>`
     width: 100%;
   }
 `;
+
+export default SectionLayout;
