dade79360611a82332f9a67d4656356090bf4f56
Michael E <adebisienads@gmail.com>
2023-09-30 07:17:44 +0100

    newsletter api
    Signed-off-by: Michael E <adebisienads@gmail.com>

--- src/components/organisms/fieldInput.tsx
+++ src/components/organisms/fieldInput.tsx
@@ -76,8 +76,18 @@ export const ErrorText = ({ text }: { text: string }) => {
 };
 
 export const FieldInput = (props: FieldProps) => {
-  const { id, name, type, placeholder, formik, addon, onChange, disabled, min, max } =
-    props;
+  const {
+    id,
+    name,
+    type,
+    placeholder,
+    formik,
+    addon,
+    onChange,
+    disabled,
+    min,
+    max,
+  } = props;
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
@@ -197,7 +207,7 @@ export const FieldString = (props: FieldProps) => {
   const error = getNestedValue(formik.errors, name);
 
   const handleChange = (e: any) => {
-    console.log('ee: ', e)
+    console.log("ee: ", e);
     formik.setFieldValue(name, e);
   };
 
@@ -207,7 +217,7 @@ export const FieldString = (props: FieldProps) => {
     <Section styles={{ position: "relative" }}>
       <SearchStringInput
         options={options}
-        onChange={e=>console.log('ee: ', e)}
+        onChange={(e) => console.log("ee: ", e)}
         placeholder={placeholder}
         value={value ? value : formikvalue}
         error={touched && error}
