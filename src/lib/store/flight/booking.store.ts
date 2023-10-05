922c67820971864beba5b9c8bab50790c26188f9
Abdulazeez Olalere <olalereazeez11@gmail.com>
2023-10-05 09:54:36 +0100

    FEAT: implement all services in booking store

--- src/lib/store/flight/booking.store.ts
+++ src/lib/store/flight/booking.store.ts
@@ -1,5 +1,12 @@
 import { FlightBookingService } from "@/lib/services/flight-booking.service";
-import { SearchFlightsRequestQuery } from "@/lib/types/request-models/flight/booking.type";
+import {
+  CheckFlightsRequestInput,
+  CheckSeatingRequestInput,
+  ConfirmPaymentZoozRequestInput,
+  SaveBookingRequestInput,
+  SearchFlightsRequestQuery,
+  TokenizeDataRequestInput,
+} from "@/lib/types/request-models/flight/booking.type";
 import { FlightInfo } from "@/lib/types/response-models/flight/booking.type";
 import { Mode } from "@lib/types";
 import { create } from "zustand";
@@ -16,10 +23,13 @@ interface State {
 interface Actions {
   prevStep: () => void;
   setStep: (params: { step: number }) => void;
-  searchFlights: ({
-    data,
-  }: {
-    data: SearchFlightsRequestQuery;
+  searchFlights: (params: { data: SearchFlightsRequestQuery }) => Promise<void>;
+  checkFlights: (params: { data: CheckFlightsRequestInput }) => Promise<void>;
+  checkSeating: (params: { data: CheckSeatingRequestInput }) => Promise<void>;
+  saveBooking: (params: { data: SaveBookingRequestInput }) => Promise<void>;
+  tokenizeData: (params: { data: TokenizeDataRequestInput }) => Promise<void>;
+  confirmPaymentZooz: (params: {
+    data: ConfirmPaymentZoozRequestInput;
   }) => Promise<void>;
 }
 
@@ -46,14 +56,104 @@ export const useFlightBookingStore = create<State & Actions>(
     },
 
     searchFlights: async ({ data }: { data: SearchFlightsRequestQuery }) => {
-      set({ mode: Mode.loading });
+      set({ searchFlightsMode: Mode.loading });
       return await FlightBookingService.searchFlights({
         data,
       })
         .then((response) => {
           set((state) => ({
-            mode: Mode.loaded,
+            searchFlightsMode: Mode.loaded,
             searchFlightsResults: response.data,
+          }));
+        })
+        .catch((error) => {
+          set({
+            searchFlightsMode: Mode.error,
+          });
+          throw error;
+        });
+    },
+    checkFlights: async ({ data }: { data: CheckFlightsRequestInput }) => {
+      set({ mode: Mode.loading });
+      return await FlightBookingService.checkFlights({
+        data,
+      })
+        .then((response) => {
+          set((state) => ({
+            mode: Mode.loaded,
+          }));
+        })
+        .catch((error) => {
+          set({
+            mode: Mode.error,
+          });
+          throw error;
+        });
+    },
+
+    checkSeating: async ({ data }: { data: CheckSeatingRequestInput }) => {
+      set({ mode: Mode.loading });
+      return await FlightBookingService.checkSeating({
+        data,
+      })
+        .then((response) => {
+          set((state) => ({
+            mode: Mode.loaded,
+          }));
+        })
+        .catch((error) => {
+          set({
+            mode: Mode.error,
+          });
+          throw error;
+        });
+    },
+    saveBooking: async ({ data }: { data: SaveBookingRequestInput }) => {
+      set({ mode: Mode.loading });
+      return await FlightBookingService.saveBooking({
+        data,
+      })
+        .then((response) => {
+          set((state) => ({
+            mode: Mode.loaded,
+          }));
+        })
+        .catch((error) => {
+          set({
+            mode: Mode.error,
+          });
+          throw error;
+        });
+    },
+    tokenizeData: async ({ data }: { data: TokenizeDataRequestInput }) => {
+      set({ mode: Mode.loading });
+      return await FlightBookingService.tokenizeData({
+        data,
+      })
+        .then(() => {
+          set((state) => ({
+            mode: Mode.loaded,
+          }));
+        })
+        .catch((error) => {
+          set({
+            mode: Mode.error,
+          });
+          throw error;
+        });
+    },
+    confirmPaymentZooz: async ({
+      data,
+    }: {
+      data: ConfirmPaymentZoozRequestInput;
+    }) => {
+      set({ mode: Mode.loading });
+      return await FlightBookingService.confirmPaymentZooz({
+        data,
+      })
+        .then((response) => {
+          set((state) => ({
+            mode: Mode.loaded,
           }));
         })
         .catch((error) => {
