e87af36d592b403e0ba60a36e7a70b8aa93cc6bc
Abdulazeez Olalere <olalereazeez11@gmail.com>
2023-10-27 11:23:31 +0100

    FEAT: update save booking payload to match API spec

--- src/lib/types/request-models/flight/booking.type.ts
+++ src/lib/types/request-models/flight/booking.type.ts
@@ -2,279 +2,284 @@ import { CountryType } from "@/components/molecules/serviceTabs/components/visa"
 import { mockCountry } from "../../schema";
 
 interface PaymentDetails {
-  status: string;
-  token: string;
-  encrypted_cvv: string;
-  bin_number: string;
-  last_4_digits: string;
-  holder_name: string;
-  expiration: string;
-  vendor: string;
-  issuer: string | null;
-  country_code: string;
-  level: string;
-  type: string;
-  pass_luhn_validation: boolean;
-  // risk_assessment: {
-  //   correlationId: string;
-  //   version: string;
-  //   status: string;
-  // };
+    status: string;
+    token: string;
+    encrypted_cvv: string;
+    bin_number: string;
+    last_4_digits: string;
+    holder_name: string;
+    expiration: string;
+    vendor: string;
+    issuer: string | null;
+    country_code: string;
+    level: string;
+    type: string;
+    pass_luhn_validation: boolean;
+    // risk_assessment: {
+    //   correlationId: string;
+    //   version: string;
+    //   status: string;
+    // };
 }
 
 export enum PassengerCategory {
-  ADULT = "adult",
-  CHILD = "child",
-  INFANT = "infant",
+    ADULT = "adult",
+    CHILD = "child",
+    INFANT = "infant",
 }
 
 export interface Passenger {
-  name: string;
-  surname: string;
-  phone: string;
-  email: string;
-  cardno: string;
-  birthday: string; // YYYY-MM-DD format
-  nationality: string; // ISO 3166-1 alpha-2 format (2 letter format)
-  title: string;
-  expiration: string; // expiration of passport, YYYY-MM-DD format
-  category: string;
+    name: string;
+    surname: string;
+    phone: string;
+    email: string;
+    cardno: string;
+    birthday: string; // YYYY-MM-DD format
+    nationality: string; // ISO 3166-1 alpha-2 format (2 letter format)
+    title: string;
+    expiration: string; // expiration of passport, YYYY-MM-DD format
+    category: string;
+    currency: string;
 }
 
 export interface PassengerFormInterface {
-  name: string;
-  surname: string;
-  phone: string;
-  email: string;
-  cardno: string;
-  birthday: string; // YYYY-MM-DD format
-  nationality: CountryType; // ISO 3166-1 alpha-2 format (2 letter format)
-  title: string;
-  issuingdate: string; // YYYY-MM-DD format
-  expiration: string; // expiration of passport, YYYY-MM-DD format
-  category: PassengerCategory;
+    name: string;
+    surname: string;
+    phone: string;
+    email: string;
+    cardno: string;
+    birthday: string; // YYYY-MM-DD format
+    nationality: CountryType; // ISO 3166-1 alpha-2 format (2 letter format)
+    title: string;
+    expiration: string; // expiration of passport, YYYY-MM-DD format
+    category: PassengerCategory;
+    currency: string;
 }
 
 export interface CombinationPrice {
-  currency: string;
-  amount: number;
-  base: number;
-  service: number;
-  service_flat: number;
-  merchant: number;
+    currency: string;
+    amount: number;
+    base: number;
+    service: number;
+    service_flat: number;
+    merchant: number;
 }
 
 export interface CombinationConditions {
-  passenger_groups: string[];
+    passenger_groups: string[];
 }
 export interface Combination {
-  indices: number[];
-  category: string;
-  conditions: CombinationConditions;
-  price: CombinationPrice;
+    indices: number[];
+    category: string;
+    conditions: CombinationConditions;
+    price: CombinationPrice;
 }
 
 export interface Baggage {
-  combination: Combination;
-  passengers: number[];
+    combination: Combination;
+    passengers: number[];
 }
 
 export interface CardInfo {
-  number: string;
-  cvv: string;
-  expirationMonth: string;
-  expirationYear: string;
-  holder: string;
+    number: string;
+    cvv: string;
+    expirationMonth: string;
+    expirationYear: string;
+    holder: string;
 }
 
 export interface SearchFlightsRequestQuery {
-  fly_from?: string;
-  fly_to?: string;
-  date_from?: string;
-  date_to?: string;
-  fly_days_type?: string;
-  fly_days?: string;
-  curr?: string;
-  adults?: number;
-  children?: number;
-  infants?: number;
-  selected_cabins?: string;
-  atime_from?: string;
-  atime_to?: string;
-  return_from?: string;
-  return_to?: string;
-  ret_dtime_from?: string;
-  ret_dtime_to?: string;
-  adult_hold_bag?: string;
-  price_from?: string;
-  price_to?: string;
-  select_airlines?: string[];
-  vehicle_type?: string;
-  max_stopovers?: string;
+    fly_from?: string;
+    fly_to?: string;
+    date_from?: string;
+    date_to?: string;
+    fly_days_type?: string;
+    fly_days?: string;
+    curr?: string;
+    adults?: number;
+    children?: number;
+    infants?: number;
+    selected_cabins?: string;
+    atime_from?: string;
+    atime_to?: string;
+    return_from?: string;
+    return_to?: string;
+    ret_dtime_from?: string;
+    ret_dtime_to?: string;
+    adult_hold_bag?: string;
+    price_from?: string;
+    price_to?: string;
+    select_airlines?: string[];
+    vehicle_type?: string;
+    max_stopovers?: string;
 }
 
 export interface CheckFlightsRequestInput {
-  booking_token: string;
-  session_id?: string;
+    booking_token: string;
+    session_id?: string;
 }
 export interface CheckFlightsQuery {
-  booking_token?: string;
-  session_id?: string;
-  bnum: number;
-  adults?: number;
-  children?: number;
-  infants?: number;
+    booking_token?: string;
+    session_id?: string;
+    bnum: number;
+    adults?: number;
+    children?: number;
+    infants?: number;
 }
 export interface CheckSeatingRequestInput {
-  ancillaries: string[];
-  booking_token: string;
-  currency: string;
-  passengers: Pick<Passenger, "birthday" | "category" | "nationality">[];
-  session_id: string;
+    ancillaries: string[];
+    booking_token: string;
+    currency: string;
+    passengers: Pick<Passenger, "birthday" | "category" | "nationality">[];
+    session_id: string;
 }
 
 interface SeatingOption {
-  segment_code: string;
-  option: string;
-  price: {
+    segment_code: string;
+    option: string;
+    price: {
+        amount: string;
+        currency: string;
+        base: string;
+        service: string;
+        service_flat: string;
+        merchant: string;
+    };
+}
+export interface SeatingSeatPrice {
     amount: string;
     currency: string;
     base: string;
     service: string;
     service_flat: string;
     merchant: string;
-  };
-}
-export interface SeatingSeatPrice {
-  amount: string;
-  currency: string;
-  base: string;
-  service: string;
-  service_flat: string;
-  merchant: string;
 }
 interface SeatingSeat {
-  seat: string;
-  passenger_idx: number;
-  price: SeatingSeatPrice;
+    seat: string;
+    passenger_idx: number;
+    price: SeatingSeatPrice;
 }
 
 export interface ParticularSeatingOption {
-  segment_code: string;
-  option: string;
-  seats: SeatingSeat[];
+    segment_code: string;
+    option: string;
+    seats: SeatingSeat[];
 }
 interface AdditionalServices {
-  seating: (SeatingOption | ParticularSeatingOption)[];
+    seating: (SeatingOption | ParticularSeatingOption)[];
 }
 export interface SaveBookingRequestInput {
-  health_declaration_checked: boolean;
-  lang: string;
-  locale: string;
-  payment_gateway: string;
-  passengers: Passenger[];
-  booking_token: string;
-  session_id: string;
-  baggage: Baggage[];
-  additional_services: AdditionalServices | null;
+    health_declaration_checked: boolean;
+    lang: string;
+    passengers: Passenger[];
+    booking_token: string;
+    session_id: string;
+    baggage: Baggage[];
+    additional_services: AdditionalServices | null;
 }
 export interface TokenizeDataRequestInput {
-  card: CardInfo;
-  payment: {
+    card: CardInfo;
+    payment: {
+        order_id: string;
+        token: string;
+        gate: string;
+        email: string;
+    };
+    booking_id: string;
     order_id: string;
-    token: string;
-    gate: string;
-    email: string;
-  };
+    paymentToken: string;
 }
 export interface ConfirmPaymentZoozRequestInput {
-  payment_details: PaymentDetails;
-  booking_id: string;
-  order_id: string;
-  paymentToken: string;
-  paymentMethodToken: string;
-  sandbox: boolean;
-  language: string;
+    payment_details: PaymentDetails;
+    booking_id: string;
+    order_id: string;
+    paymentToken: string;
+    paymentMethodToken: string;
+    sandbox: boolean;
+    language: string;
 }
 export interface PassengerBaggageCombinationInterface {
-  hand_bag: Combination;
-  hold_bag: Combination;
+    hand_bag: Combination;
+    hold_bag: Combination;
 }
 
 export const arrangeBaggageDataForOrdering = (
-  passengers: PassengerBaggageCombinationInterface[]
+    passengers: PassengerBaggageCombinationInterface[]
 ): Baggage[] => {
-  let baggageData: Baggage[] = [];
+    let baggageData: Baggage[] = [];
 
-  const checkIfBaggageCombinationIsAlreadyRegistered = (indices: number[]) => {
-    return baggageData.findIndex(
-      (baggageGroup) =>
-        baggageGroup.combination.indices.toString() == indices.toString()
-    );
-  };
+    const checkIfBaggageCombinationIsAlreadyRegistered = (
+        indices: number[]
+    ) => {
+        return baggageData.findIndex(
+            (baggageGroup) =>
+                baggageGroup.combination.indices.toString() ==
+                indices.toString()
+        );
+    };
 
-  const arrangeForBaggageType = ({
-    type,
-  }: {
-    type: "hand_bag" | "hold_bag";
-  }) => {
-    passengers.forEach((passenger, passengerIndex) => {
-      console.log(passenger);
-      console.log("  passenger[type].indices", passenger[type]);
-      const bagCombinationIndex = checkIfBaggageCombinationIsAlreadyRegistered(
-        passenger[type].indices
-      );
-      if (bagCombinationIndex == -1) {
-        baggageData = [
-          ...baggageData,
-          {
-            combination: passenger[type],
-            passengers: [passengerIndex],
-          },
-        ];
-      } else {
-        baggageData[bagCombinationIndex].passengers = [
-          ...baggageData[bagCombinationIndex].passengers,
-          passengerIndex,
-        ];
-      }
-    });
-  };
+    const arrangeForBaggageType = ({
+        type,
+    }: {
+        type: "hand_bag" | "hold_bag";
+    }) => {
+        passengers.forEach((passenger, passengerIndex) => {
+            console.log(passenger);
+            console.log("  passenger[type].indices", passenger[type]);
+            const bagCombinationIndex =
+                checkIfBaggageCombinationIsAlreadyRegistered(
+                    passenger[type].indices
+                );
+            if (bagCombinationIndex == -1) {
+                baggageData = [
+                    ...baggageData,
+                    {
+                        combination: passenger[type],
+                        passengers: [passengerIndex],
+                    },
+                ];
+            } else {
+                baggageData[bagCombinationIndex].passengers = [
+                    ...baggageData[bagCombinationIndex].passengers,
+                    passengerIndex,
+                ];
+            }
+        });
+    };
 
-  arrangeForBaggageType({ type: "hold_bag" });
-  arrangeForBaggageType({ type: "hand_bag" });
+    arrangeForBaggageType({ type: "hold_bag" });
+    arrangeForBaggageType({ type: "hand_bag" });
 
-  return baggageData;
+    return baggageData;
 };
 export const passengerAndBaggageDetails: PassengerFormInterface = {
-  name: "Abd",
-  surname: "a",
-  phone: "+2349088990012",
-  email: "oallere@hjdsaol.com",
-  cardno: "D25845822",
-  birthday: "1998-12-10",
-  nationality: { code: "NG", name: "Nigeria", flag: "s" },
-  title: "Mr",
-  issuingdate: "2023-12-10",
-  expiration: "2030-12-10",
-  category: PassengerCategory.ADULT,
+    name: "Abd",
+    surname: "a",
+    phone: "+2349088990012",
+    email: "oallere@hjdsaol.com",
+    cardno: "D25845822",
+    birthday: "1998-12-10",
+    nationality: { code: "NG", name: "Nigeria", flag: "s" },
+    title: "Mr",
+    expiration: "2030-12-10",
+    category: PassengerCategory.ADULT,
+    currency: "usd",
 };
 export const saveBookingDetails: SaveBookingRequestInput = {
-  health_declaration_checked: true,
-  lang: "en",
-  locale: "en",
-  payment_gateway: "payu",
-  passengers: [],
-  booking_token: "",
-  session_id: "",
-  baggage: [],
-  additional_services: null,
+    health_declaration_checked: true,
+    lang: "en",
+
+    passengers: [],
+    booking_token: "",
+    session_id: "",
+    baggage: [],
+    additional_services: null,
 };
 
 export const cardDetails: CardInfo = {
-  number: "",
-  cvv: "",
-  expirationMonth: "",
-  expirationYear: "",
-  holder: "",
+    number: "",
+    cvv: "",
+    expirationMonth: "",
+    expirationYear: "",
+    holder: "",
 };
