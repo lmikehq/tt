a0522f788a9eb757a5b67f7b630a30e285cd539d
Abdulazeez Olalere <olalereazeez11@gmail.com>
2023-11-23 13:10:51 +0100

    FEAT: re-implement stays search

--- src/components/molecules/serviceTabs/components/stays.tsx
+++ src/components/molecules/serviceTabs/components/stays.tsx
@@ -24,315 +24,270 @@ import { constructQueryFromParams } from "@/lib/extensions/helpers/constructQuer
 import { formatDate } from "@/lib/utilFns";
 // STYLES
 const FlexBox = styled.div`
-  display: flex;
-  align-items: center;
-  max-width: 50%;
-  justify-content: flex-start;
-  gap: 10px;
-  margin-bottom: 20px;
+    display: flex;
+    align-items: center;
+    max-width: 50%;
+    justify-content: flex-start;
+    gap: 10px;
+    margin-bottom: 20px;
 `;
 const FlexItems = styled.div``;
 
 const Span = styled.div`
-  position: relative;
+    position: relative;
 `;
 
-export type RoomCountType = {
-  adults: number;
-  children: number;
-  infants: number;
-  rooms: number;
-};
-
-const room: RoomCountType = {
-  adults: 2,
-  children: 1,
-  infants: 0,
-  rooms: 1,
-};
-
-export interface RoomStay {
-  roomCount: RoomCountType;
-  data: string;
-  destinationHotel: Location;
-}
-
 function Stays() {
-  const [data, setData] = useState("");
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const [loading, setLoading] = useState<boolean>(false);
-  const router = useRouter();
-  const { isMobile } = useScreenResolution();
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const [loading, setLoading] = useState<boolean>(false);
+    const router = useRouter();
+    const { isMobile } = useScreenResolution();
 
-  const today = dayjs().toDate();
+    const today = dayjs().toDate();
 
-  // FORMAT TEXT
-  const formatDisplayText = (data: RoomCountType) => {
-    const kids = data.children + data.infants;
-    return `${data.adults} ${data.adults > 1 ? "Adults" : "Adult"}${
-      kids > 0 ? `, ${kids} ${kids === 1 ? "Child" : "Children"}` : ""
-    }
-    `;
-  };
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
 
-  const defText = formatDisplayText(room);
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
-  const [displayText, setDisplayText] = useState(defText);
+    const open = Boolean(anchorEl);
+    const { stayTabInitialSearchQuery, updateStaySearchSearchFilter } =
+        useStaySearchStore((state) => state);
 
-  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
+    const { roomForGuests } = stayTabInitialSearchQuery;
+    const validateStaySearchFilter =
+        stayTabInitialSearchQuery.location &&
+        stayTabInitialSearchQuery.checkInDate &&
+        stayTabInitialSearchQuery.checkOutDate &&
+        stayTabInitialSearchQuery.roomForGuests.length > 0
+            ? true
+            : false;
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+    const computeStaySearchQuery = () => {
+        const params = {
+            location: stayTabInitialSearchQuery.location?.code,
+            checkIn: formatDate(
+                stayTabInitialSearchQuery.checkInDate ?? dayjs()
+            ),
+            checkOut: formatDate(
+                stayTabInitialSearchQuery.checkOutDate ?? dayjs()
+            ),
+            guests:
+                roomForGuests.length == 1
+                    ? roomForGuests[0].adults + roomForGuests[0].children
+                    : `${roomForGuests.length}-${roomForGuests
+                          .map((el) => el.adults + el.children)
+                          .join(",")}`,
+        };
 
-  const handleDataChange = (data: any) => {
-    setData(
-      `${data.count.adults} Adult, ${data.count.children} Children, ${data.count.rooms} Rooms`
+        return constructQueryFromParams(params);
+    };
+    const computeGuestsAndRoomsString = () => {
+        const rooms = roomForGuests.length;
+        let guests = 0;
+
+        for (let index = 0; index < roomForGuests.length; index++) {
+            const room = roomForGuests[index];
+            guests += room.adults + room.children;
+        }
+        return `${rooms} room${rooms == 1 ? "" : "s"} for ${guests} guest${
+            guests == 1 ? "" : "s"
+        }`;
+    };
+
+    return (
+        <Section
+            padding={"2rem 0 1rem 0"}
+            height="unset"
+            styles={{ position: "relative" }}
+        >
+            <FlexBox>
+                <FlexItems>
+                    <FormControlLabel
+                        control={
+                            <Checkbox
+                                disableFocusRipple
+                                disableRipple
+                                sx={{
+                                    color: ttColors.primary,
+                                    "&.Mui-checked": {
+                                        color: ttColors.primary,
+                                    },
+                                }}
+                            />
+                        }
+                        label="Free Cancellation"
+                    />
+                </FlexItems>
+                <FlexItems>
+                    <FormControlLabel
+                        control={
+                            <Checkbox
+                                disableFocusRipple
+                                disableRipple
+                                sx={{
+                                    color: ttColors.primary,
+                                    "&.Mui-checked": {
+                                        color: ttColors.primary,
+                                    },
+                                }}
+                            />
+                        }
+                        label="4 stars"
+                    />
+                </FlexItems>
+                <FlexItems>
+                    <FormControlLabel
+                        control={
+                            <Checkbox
+                                disableFocusRipple
+                                disableRipple
+                                sx={{
+                                    color: ttColors.primary,
+                                    "&.Mui-checked": {
+                                        color: ttColors.primary,
+                                    },
+                                }}
+                            />
+                        }
+                        label="3 stars"
+                    />
+                </FlexItems>
+            </FlexBox>
+            <Flex
+                align="center"
+                direction={isMobile ? "column" : "row"}
+                gap=".5rem"
+                styles={{ paddingBottom: "20px" }}
+            >
+                <Flex
+                    direction="column"
+                    gap=".5rem"
+                    styles={{ marginBottom: isMobile ? "1.2rem" : "0" }}
+                >
+                    <Text
+                        type="label"
+                        size={isMobile ? 16 : 18}
+                        text="Your stay preference?"
+                    />
+                    <Span>
+                        <LocationSearchSelectInput
+                            onChange={(x: Location) =>
+                                updateStaySearchSearchFilter({
+                                    ...stayTabInitialSearchQuery,
+                                    location: x,
+                                })
+                            }
+                            value={stayTabInitialSearchQuery.location}
+                            placeholder="Enter Destination or Hotel Name"
+                        />
+                    </Span>
+                </Flex>
+                <Flex
+                    direction="column"
+                    gap=".5rem"
+                    styles={{ marginBottom: isMobile ? "1.2rem" : "0" }}
+                >
+                    <Text
+                        type="label"
+                        size={isMobile ? 16 : 18}
+                        text="Check In"
+                    />
+                    <DatePicker
+                        placeholder="Select Date"
+                        minDate={today}
+                        value={stayTabInitialSearchQuery.checkInDate?.toDate()}
+                        onChange={(e) =>
+                            updateStaySearchSearchFilter({
+                                ...stayTabInitialSearchQuery,
+                                checkInDate: dayjs(e),
+                            })
+                        }
+                    />
+                </Flex>
+
+                <Flex
+                    direction="column"
+                    gap=".5rem"
+                    styles={{ marginBottom: isMobile ? "1.2rem" : "0" }}
+                >
+                    <Text
+                        type="label"
+                        size={isMobile ? 16 : 18}
+                        text="Return"
+                    />
+                    <DatePicker
+                        placeholder="Select Date"
+                        minDate={today}
+                        value={stayTabInitialSearchQuery.checkOutDate?.toDate()}
+                        onChange={(e) =>
+                            updateStaySearchSearchFilter({
+                                ...stayTabInitialSearchQuery,
+                                checkOutDate: dayjs(e),
+                            })
+                        }
+                    />
+                </Flex>
+
+                <Flex
+                    direction="column"
+                    gap=".75rem"
+                    styles={{ marginBottom: isMobile ? "1.2rem" : "0" }}
+                >
+                    <Text
+                        type="label"
+                        size={isMobile ? 16 : 18}
+                        text="Guests and Rooms"
+                    />
+                    <ClickAwayListener onClickAway={handleClose}>
+                        <div>
+                            <Input
+                                onClick={handleClick}
+                                placeholder="Click me to open dropdown"
+                                value={computeGuestsAndRoomsString()}
+                                styles={{
+                                    fontFamily: "poppins",
+                                    cursor: "pointer",
+                                    fontSize: "14px",
+                                }}
+                            />
+                            {open && <StaysMenu />}
+                        </div>
+                    </ClickAwayListener>
+                </Flex>
+            </Flex>
+
+            <Flex
+                justify="flex-end"
+                margin={isMobile ? "1rem 0 0" : "1.5rem 0 0"}
+            >
+                <Button
+                    width={isMobile ? "100%" : "300px"}
+                    padding="0 1.5rem"
+                    borderRadius="4px"
+                    background={ttColors.dark}
+                    disabled={!validateStaySearchFilter}
+                    onClick={async () => {
+                        if (loading) return;
+                        setLoading(true);
+                        await sleep(200);
+                        router.push(
+                            `/stay/listings${computeStaySearchQuery()}`
+                        );
+                    }}
+                >
+                    {loading ? (
+                        <Spinner fill={ttColors.primary} size={"36px"} />
+                    ) : (
+                        <Text type="p" text="Search for Hotels" weight={500} />
+                    )}
+                </Button>
+            </Flex>
+        </Section>
     );
-  };
-
-  // LOCATION SEARCH
-  const [stay, setStay] = useState<RoomStay>({
-    roomCount: {
-      adults: 2,
-      children: 1,
-      infants: 0,
-      rooms: 1,
-    },
-    data: "",
-    destinationHotel: {
-      id: "ibadan-id",
-      int_id: 123,
-      airport_int_id: 456,
-      active: true,
-      code: "IBD",
-      icao: "ABC123",
-      name: "",
-      slug: "ibadan",
-      slug_en: "ibadan-en",
-      alternative_names: [],
-      rank: 1,
-      global_rank_dst: 1,
-      dst_popularity_score: 0.8,
-      timezone: "Africa/Lagos",
-      country: {
-        id: "nigeria-id",
-        name: "Nigeria",
-        slug: "nigeria",
-        code: "NG",
-      },
-      city: {
-        id: "ibadan-city-id",
-        name: "Ibadan",
-        code: "IBD",
-        nearby_country: null,
-        slug: "ibadan",
-        subdivision: null,
-        autonomous_territory: null,
-        country: {
-          id: "nigeria-id",
-          name: "Nigeria",
-          slug: "nigeria",
-          code: "NG",
-        },
-        region: {
-          id: "ibadan-region-id",
-          name: "Ibadan Region",
-          slug: "ibadan-region",
-        },
-        continent: {
-          id: "africa-id",
-          name: "Africa",
-          slug: "africa",
-          code: "AF",
-        },
-      },
-      location: {
-        lon: "7.4951",
-        lat: "4.3517",
-      },
-      alternative_departure_points: [],
-      tags: [],
-      providers: [],
-      special: [],
-      tourist_region: [],
-      car_rentals: [],
-      new_ground: false,
-      example: false,
-      routing_priority: 1,
-      type: "city",
-    },
-  });
-
-  const handleUpdate = (
-    selectedStay: RoomStay,
-    updatedData: { destinationHotel: Location }
-  ) => {
-    setStay({ ...selectedStay, ...updatedData });
-  };
-
-  const open = Boolean(anchorEl);
-
-  return (
-    <Section
-      padding={"2rem 0 1rem 0"}
-      height="unset"
-      styles={{ position: "relative" }}
-    >
-      <FlexBox>
-        <FlexItems>
-          <FormControlLabel
-            control={
-              <Checkbox
-                disableFocusRipple
-                disableRipple
-                sx={{
-                  "&.Mui-checked": {
-                    color: ttColors.primary,
-                  },
-                }}
-              />
-            }
-            label="Free Cancellation"
-          />
-        </FlexItems>
-        <FlexItems>
-          <FormControlLabel
-            control={
-              <Checkbox
-                disableFocusRipple
-                disableRipple
-                sx={{
-                  "&.Mui-checked": {
-                    color: ttColors.primary,
-                  },
-                }}
-              />
-            }
-            label="4 stars"
-          />
-        </FlexItems>
-        <FlexItems>
-          <FormControlLabel
-            control={
-              <Checkbox
-                disableFocusRipple
-                disableRipple
-                sx={{
-                  "&.Mui-checked": {
-                    color: ttColors.primary,
-                  },
-                }}
-              />
-            }
-            label="3 stars"
-          />
-        </FlexItems>
-      </FlexBox>
-      <Flex
-        align="center"
-        direction={isMobile ? "column" : "row"}
-        gap=".5rem"
-        styles={{ paddingBottom: "20px" }}
-      >
-        <Flex
-          direction="column"
-          gap=".5rem"
-          styles={{ marginBottom: isMobile ? "1.2rem" : "0" }}
-        >
-          <Text
-            type="label"
-            size={isMobile ? 16 : 18}
-            text="Your stay preference?"
-          />
-          <Span>
-            <LocationSearchSelectInput
-              onChange={(x: Location) =>
-                handleUpdate && handleUpdate(stay, { destinationHotel: x })
-              }
-              value={stay.destinationHotel}
-              placeholder="Enter Destination or Hotel Name"
-            />
-          </Span>
-        </Flex>
-        <Flex
-          direction="column"
-          gap=".5rem"
-          styles={{ marginBottom: isMobile ? "1.2rem" : "0" }}
-        >
-          <Text type="label" size={isMobile ? 16 : 18} text="Check In" />
-          <DatePicker
-            placeholder="Select Date"
-            minDate={today}
-            onChange={(e) => null}
-          />
-        </Flex>
-
-        <Flex
-          direction="column"
-          gap=".5rem"
-          styles={{ marginBottom: isMobile ? "1.2rem" : "0" }}
-        >
-          <Text type="label" size={isMobile ? 16 : 18} text="Return" />
-          <DatePicker
-            placeholder="Select Date"
-            minDate={today}
-            onChange={(e) => null}
-          />
-        </Flex>
-
-        <Flex
-          direction="column"
-          gap=".75rem"
-          styles={{ marginBottom: isMobile ? "1.2rem" : "0" }}
-        >
-          <Text
-            type="label"
-            size={isMobile ? 16 : 18}
-            text="Guests and Rooms"
-          />
-          <ClickAwayListener onClickAway={handleClose}>
-            <div>
-              <Input
-                onClick={handleClick}
-                placeholder="Click me to open dropdown"
-                value={displayText}
-                styles={{
-                  fontFamily: "poppins",
-                  cursor: "pointer",
-                  fontSize: "14px",
-                }}
-              />
-              {open && <StaysMenu />}
-            </div>
-          </ClickAwayListener>
-        </Flex>
-      </Flex>
-
-      <Flex justify="flex-end" margin={isMobile ? "1rem 0 0" : "1.5rem 0 0"}>
-        <Button
-          width={isMobile ? "100%" : "300px"}
-          padding="0 1.5rem"
-          borderRadius="4px"
-          background={ttColors.dark}
-          onClick={async () => {
-            if (loading) return;
-            setLoading(true);
-            await sleep(200);
-            router.push("/stay/listings");
-          }}
-        >
-          {loading ? (
-            <Spinner fill={ttColors.primary} size={"36px"} />
-          ) : (
-            <Text type="p" text="Search for Hotels" weight={500} />
-          )}
-        </Button>
-      </Flex>
-    </Section>
-  );
 }
 
 export default Stays;
