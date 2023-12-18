eb60b4c5ae3ef511534198441e906312a7670da7
dayankoya <dbayankoya@gmail.com>
2023-12-18 09:24:48 +0100

    update: add login modal to visa application

--- src/components/molecules/dashboardTabs/dashboard.tsx
+++ src/components/molecules/dashboardTabs/dashboard.tsx
@@ -1,17 +1,26 @@
+'use client'
+
+import { useUserStore } from "@/lib/store/useStore";
 import SectionLayout from "@components/templates/SectionLayout";
+import { redirect } from "next/navigation";
 import DashboardTabs from "src/components/molecules/dashboardTabs";
 import UserPicture from "src/components/molecules/dashboardTabs/components/picture";
 import Section from "src/components/molecules/section";
 
 const DashboardHeader = () => {
-  return (
-    <Section className="dashboardWrapper" margin={"1rem 0px"}>
-      <SectionLayout>
-        <UserPicture />
-        <DashboardTabs />
-      </SectionLayout>
-    </Section>
-  );
+    const { user } = useUserStore()
+    if (!user?._id) {
+        redirect('/login')
+    }
+
+    return (
+        <Section className="dashboardWrapper" margin={"1rem 0px"}>
+            <SectionLayout>
+            <UserPicture />
+            <DashboardTabs />
+            </SectionLayout>
+        </Section>
+    );
 };
 
 export default DashboardHeader;
