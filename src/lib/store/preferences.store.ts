8afc552dcda59f3e596f767ad676a5782457a88d
Ola Josh <akandetunji3@gmail.com>
2023-12-20 23:36:53 +0100

    FEAT: Modal currency update

--- src/lib/store/preferences.store.ts
+++ src/lib/store/preferences.store.ts
@@ -1,35 +1,42 @@
 import { create } from "zustand";
 
 interface State {
-    preFerredCurrency: string;
-    preferredLanguage: string;
-    conversionRate: number;
-    showBackDropLoader: boolean;
-}
-interface Actions {
-    setConversionRate: (value: number) => void;
-    setPreferredCurrency: (value: string) => void;
-    setShowBackDropLoader: (value: boolean) => void;
+  preFerredCurrency: string;
+  preferredLanguage: string;
+  conversionRate: number;
+  showBackDropLoader: boolean;
 }
 
+interface Actions {
+  setConversionRate: (value: number) => void;
+  setPreferredCurrency: (value: string) => void;
+  setShowBackDropLoader: (value: boolean) => void;
+}
+
+// SELECTED CURRENCY
+const selectedCurrency =
+  typeof window !== "undefined"
+    ? localStorage.getItem("selectedCurrency") || "NGN"
+    : "NGN";
+
 export const useUserPreferencesStore = create<State & Actions>(
-    (set): State & Actions => ({
-        preFerredCurrency: "NGN",
-        preferredLanguage: "en",
-        conversionRate: 0,
-        showBackDropLoader: false,
-        setConversionRate: (value: number) => {
-            set({ conversionRate: value });
-        },
-        setShowBackDropLoader: (value: boolean) => {
-            set({
-                showBackDropLoader: value,
-            });
-        },
-        setPreferredCurrency: (value: string) => {
-            set({
-                preFerredCurrency: value,
-            });
-        },
-    })
+  (set): State & Actions => ({
+    preFerredCurrency: selectedCurrency,
+    preferredLanguage: "en",
+    conversionRate: 0,
+    showBackDropLoader: false,
+    setConversionRate: (value: number) => {
+      set({ conversionRate: value });
+    },
+    setShowBackDropLoader: (value: boolean) => {
+      set({
+        showBackDropLoader: value,
+      });
+    },
+    setPreferredCurrency: (value: string) => {
+      set({
+        preFerredCurrency: value,
+      });
+    },
+  })
 );
