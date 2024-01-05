afcd4b45abc164316c1fdc5c34b2152431768ac6
Abdulazeez Olalere <olalereazeez11@gmail.com>
2024-01-05 17:12:10 +0100

    FEAT: resolve node module error

--- src/app/blog/search/page.tsx
+++ src/app/blog/search/page.tsx
@@ -1,89 +1,12 @@
-import Image from "@/components/atoms/image";
-import Text from "@/components/atoms/text";
-import Flex from "@/components/templates/flex";
-import BlogTab from "@/components/atoms/blogTab";
-import { useScreenResolution } from "@/lib/extensions/hook/useScreenResolution";
-import { BlogArticleMini } from "@/components/molecules/blog/component/blogArticleMini";
-import { BlogArticle } from "@/components/molecules/blog/component/blogArticle";
-import { RelatedTopic } from "@/components/molecules/blog/component/relatedTopic";
+import BlogResults from "@/components/organisms/blog/BlogResults";
+import SectionLayout from "@/components/templates/SectionLayout";
 
-function ResultPage() {
-  const { isMobile } = useScreenResolution();
-
-  const tabItems = [
-    {
-      label: "For You",
-      value: 0,
-      content: "",
-    },
-    {
-      label: "About Thrillers",
-      value: 1,
-      content: "",
-    },
-
-    {
-      label: "Visa Application",
-      value: 2,
-      content: "",
-    },
-
-    {
-      label: "Flight",
-      value: 3,
-      content: "",
-    },
-    {
-      label: "Stays",
-      value: 4,
-      content: "",
-    },
-  ];
-
-  return (
-    <>
-      <Flex gap="50px" direction={isMobile ? "column" : "row"}>
-        <Flex direction="column" width={isMobile ? "100%" : "65%"} gap="2rem">
-          <BlogTab tabItems={tabItems} addColor width={true} />
-
-          <BlogArticleMini />
-          <br />
-
-          <BlogArticleMini />
-          <br />
-
-          <BlogArticleMini />
-          <br />
-
-          <BlogArticleMini />
-          <br />
-
-          <BlogArticleMini />
-        </Flex>
-
-        <Flex width={isMobile ? "100%" : "40%"} direction="column" justify="flex-start" gap="3.5rem">
-          <Flex direction="column" gap="3rem">
-            <Text
-              type="h2"
-              text="Similar Blog Post"
-              size="20px"
-              weight={600}
-              styles={{
-                justifyContent: "center",
-                alignContent: "center",
-              }}
-            />
-
-            <BlogArticle />
-            <BlogArticle />
-            <BlogArticle />
-            <BlogArticle />
-            <BlogArticle />
-          </Flex>
-          <RelatedTopic />
-        </Flex>
-      </Flex>
-    </>
-  );
+const ResultPage = () => {
+    return (
+        <SectionLayout>
+            <BlogResults />
+        </SectionLayout>
+    );
 };
+
 export default ResultPage;
--- src/components/molecules/blog/component/heroSection.tsx
+++ src/components/molecules/blog/component/heroSection.tsx
@@ -8,199 +8,215 @@ import { useState } from "react";
 import { AiOutlineClockCircle } from "react-icons/ai";
 import { LiaTimesSolid } from "react-icons/lia";
 import { useScreenResolution } from "@/lib/extensions/hook/useScreenResolution";
-import ResultPage from "@/app/blog/search/page";
-
+import BlogResults from "@/components/organisms/blog/BlogResults";
 
 const Box = styled.div`
-  width: 886px;
-  @media (max-width: 900px) {
-    width: 100%;
-  }
+    width: 886px;
+    @media (max-width: 900px) {
+        width: 100%;
+    }
 `;
 
 const SearchArea = styled.div`
-  position: relative;
-  width: 652px;
-  border: 1px solid #b6b6b6;
-  border-radius: 8px;
-  background-color: #ffffff;
-  top: 30px;
-  height: 72px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0px 23px;
-  margin: 3rem 0rem 6rem;
+    position: relative;
+    width: 652px;
+    border: 1px solid #b6b6b6;
+    border-radius: 8px;
+    background-color: #ffffff;
+    top: 30px;
+    height: 72px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0px 23px;
+    margin: 3rem 0rem 6rem;
 
-  &:focus-within {
-    border-bottom-right-radius: 0;
-    border-bottom-left-radius: 0;
-  }
+    &:focus-within {
+        border-bottom-right-radius: 0;
+        border-bottom-left-radius: 0;
+    }
 
-  @media (max-width: 900px) {
-    width: 384px;
-  }
+    @media (max-width: 900px) {
+        width: 384px;
+    }
 `;
 
 const SearchResultsContainer = styled.div`
-  position: absolute;
-  top: calc(100% + 0px);
-  left: -1px;
-  width: 652px;
-  display: flex;
-  flex-direction: column;
-  background: #ffffff;
-  border: 1px solid #b6b6b6;
-  border-radius: 8px;
-  border-top: none;
-  border-top-right-radius: 0;
-  border-top-left-radius: 0;
-  z-index: 999;
-  padding: 0px 23px;
+    position: absolute;
+    top: calc(100% + 0px);
+    left: -1px;
+    width: 652px;
+    display: flex;
+    flex-direction: column;
+    background: #ffffff;
+    border: 1px solid #b6b6b6;
+    border-radius: 8px;
+    border-top: none;
+    border-top-right-radius: 0;
+    border-top-left-radius: 0;
+    z-index: 999;
+    padding: 0px 23px;
 
-  @media (min-width: 900px){
-    width: 100%;
-  }
+    @media (min-width: 900px) {
+        width: 100%;
+    }
 `;
 
 const SearchResultItem = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 15px 0;
-  // border-bottom: 1px solid #e0e0e0;
-  cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 15px 0;
+    // border-bottom: 1px solid #e0e0e0;
+    cursor: pointer;
 `;
 
 export const BlogHeroSection = () => {
-  const { isMobile } = useScreenResolution();
+    const { isMobile } = useScreenResolution();
 
-  const [searchTerm, setSearchTerm] = useState<string>("");
-  const [searchTriggered, setSearchTriggered] = useState(false);
-  const [searchResultText, setSearchResultText] = useState("");
-  const [recentSearches, setRecentSearches] = useState<string[]>([]);
-  const [isSearchResultsVisible, setSearchResultsVisible] = useState(false);
+    const [searchTerm, setSearchTerm] = useState<string>("");
+    const [searchTriggered, setSearchTriggered] = useState(false);
+    const [searchResultText, setSearchResultText] = useState("");
+    const [recentSearches, setRecentSearches] = useState<string[]>([]);
+    const [isSearchResultsVisible, setSearchResultsVisible] = useState(false);
 
-  // const handleToggleSearchResults = () => {
-  //   setSearchResultsVisible(!isSearchResultsVisible);
-  // };
+    // const handleToggleSearchResults = () => {
+    //   setSearchResultsVisible(!isSearchResultsVisible);
+    // };
 
-  const handleSearch = () => {
-    setSearchTriggered(true);
-    setSearchResultText(
-      searchTerm.charAt(0).toUpperCase() + searchTerm.slice(1)
-    );
+    const handleSearch = () => {
+        setSearchTriggered(true);
+        setSearchResultText(
+            searchTerm.charAt(0).toUpperCase() + searchTerm.slice(1)
+        );
 
-    if (searchTerm && !recentSearches.includes(searchTerm)) {
-      setRecentSearches([searchTerm, ...recentSearches.slice(0, 4)]);
-    }
-  };
+        if (searchTerm && !recentSearches.includes(searchTerm)) {
+            setRecentSearches([searchTerm, ...recentSearches.slice(0, 4)]);
+        }
+    };
 
-  const handleRecentSearch = (term: string) => {
-    setSearchTerm(term);
-    handleSearch();
-    setSearchResultsVisible(false);
-  };
+    const handleRecentSearch = (term: string) => {
+        setSearchTerm(term);
+        handleSearch();
+        setSearchResultsVisible(false);
+    };
 
-  const handleClearRecentSearch = (index: number) => {
-    const updatedSearches = [...recentSearches];
-    updatedSearches.splice(index, 1);
-    setRecentSearches(updatedSearches);
+    const handleClearRecentSearch = (index: number) => {
+        const updatedSearches = [...recentSearches];
+        updatedSearches.splice(index, 1);
+        setRecentSearches(updatedSearches);
 
-    setSearchResultsVisible(updatedSearches.length > 0);
-  };
+        setSearchResultsVisible(updatedSearches.length > 0);
+    };
 
-  const handleInputFocus = () => {
-    setSearchResultsVisible(true);
-  };
+    const handleInputFocus = () => {
+        setSearchResultsVisible(true);
+    };
 
-  const handleInputBlur = () => {
-    setSearchResultsVisible(false);
-  };
+    const handleInputBlur = () => {
+        setSearchResultsVisible(false);
+    };
 
-  const renderContent = () => {
-    if (searchTriggered) {
-      return <ResultPage />;
-    } else {
-      return <AdminPost />;
-    }
-  };
+    const renderContent = () => {
+        if (searchTriggered) {
+            return <BlogResults />;
+        } else {
+            return <AdminPost />;
+        }
+    };
 
-  return (
-    <Flex direction="column" justify="flex-start" align="flex-start">
-      <Flex direction="column" justify="flex-start" align="center">
-        <Text
-          type="p"
-          text="Blog Stories"
-          weight={400}
-          size={isMobile ? 16 : 24}
-          color="#06062A"
-          styles={{ lineHeight: "36px" }}
-        />
+    return (
+        <Flex direction="column" justify="flex-start" align="flex-start">
+            <Flex direction="column" justify="flex-start" align="center">
+                <Text
+                    type="p"
+                    text="Blog Stories"
+                    weight={400}
+                    size={isMobile ? 16 : 24}
+                    color="#06062A"
+                    styles={{ lineHeight: "36px" }}
+                />
 
-        <Box>
-          <Text
-            type="h1"
-            text="ENJOY TRAVEL EXPERIENCE IN FORM OF A STORY"
-            weight={700}
-            size={isMobile ? 28 : 64}
-            styles={{ lineHeight: isMobile ? "42px" : "96px", textAlign: "center" }}
-            width={isMobile ? "384px" : "886px"}
-          />
-        </Box>
+                <Box>
+                    <Text
+                        type="h1"
+                        text="ENJOY TRAVEL EXPERIENCE IN FORM OF A STORY"
+                        weight={700}
+                        size={isMobile ? 28 : 64}
+                        styles={{
+                            lineHeight: isMobile ? "42px" : "96px",
+                            textAlign: "center",
+                        }}
+                        width={isMobile ? "384px" : "886px"}
+                    />
+                </Box>
 
-        <SearchArea onFocus={handleInputFocus} onBlur={handleInputBlur}>
-          <Input
-            placeholder="Search for Blog Stories"
-            styles={{ border: "none", padding: "0px" }}
-            color="#929292"
-            width={isMobile ? "300px" : "575px"}
-            size="18px"
-            weight="400px"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                handleSearch();
-              }
-            }}
-          />
-          {isSearchResultsVisible && recentSearches.length > 0 && (
-            <SearchResultsContainer>
-              {recentSearches.map((term, index) => (
-                <SearchResultItem
-                  key={index}
-                  onClick={() => handleRecentSearch(term)}
+                <SearchArea onFocus={handleInputFocus} onBlur={handleInputBlur}>
+                    <Input
+                        placeholder="Search for Blog Stories"
+                        styles={{ border: "none", padding: "0px" }}
+                        color="#929292"
+                        width={isMobile ? "300px" : "575px"}
+                        size="18px"
+                        weight="400px"
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                        onKeyDown={(e) => {
+                            if (e.key === "Enter") {
+                                handleSearch();
+                            }
+                        }}
+                    />
+                    {isSearchResultsVisible && recentSearches.length > 0 && (
+                        <SearchResultsContainer>
+                            {recentSearches.map((term, index) => (
+                                <SearchResultItem
+                                    key={index}
+                                    onClick={() => handleRecentSearch(term)}
+                                >
+                                    <Flex
+                                        direction="row"
+                                        align="center"
+                                        gap="10px"
+                                    >
+                                        <AiOutlineClockCircle />
+                                        <span>{term}</span>
+                                    </Flex>
+                                    <LiaTimesSolid
+                                        onClick={() =>
+                                            handleClearRecentSearch(index)
+                                        }
+                                    />
+                                </SearchResultItem>
+                            ))}
+                        </SearchResultsContainer>
+                    )}
+
+                    <CiSearch
+                        size="22.17px"
+                        color="#B6B6B6"
+                        onClick={handleSearch}
+                        style={{ cursor: "pointer" }}
+                    />
+                </SearchArea>
+            </Flex>
+            {searchResultText && (
+                <h2
+                    style={{
+                        fontSize: "40px",
+                        fontWeight: "600",
+                        marginBottom: "4rem",
+                    }}
                 >
-                  <Flex direction="row" align="center" gap="10px">
-                    <AiOutlineClockCircle />
-                    <span>{term}</span>
-                  </Flex>
-                  <LiaTimesSolid
-                    onClick={() => handleClearRecentSearch(index)}
-                  />
-                </SearchResultItem>
-              ))}
-            </SearchResultsContainer>
-          )}
-
-          <CiSearch
-            size="22.17px"
-            color="#B6B6B6"
-            onClick={handleSearch}
-            style={{ cursor: "pointer" }}
-          />
-        </SearchArea>
-      </Flex>
-      {searchResultText && (
-        <h2
-          style={{ fontSize: "40px", fontWeight: "600", marginBottom: "4rem" }}
-        >
-          <span style={{ color: "#929292" }}>Search Results for </span>
-          <strong style={{ color: "#121212" }}>{searchResultText}.</strong>
-        </h2>
-      )}
-      {renderContent()}
-    </Flex>
-  );
+                    <span style={{ color: "#929292" }}>
+                        Search Results for{" "}
+                    </span>
+                    <strong style={{ color: "#121212" }}>
+                        {searchResultText}.
+                    </strong>
+                </h2>
+            )}
+            {renderContent()}
+        </Flex>
+    );
 };
