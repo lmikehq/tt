74621564c458a551a13ab2bccccd0c54815c7eec
Abdulazeez Olalere <olalereazeez11@gmail.com>
2024-01-22 07:13:41 +0100

    FEAT: Create multicity fetch hook

--- src/lib/hooks/flight/multi.hook.ts
+++ src/lib/hooks/flight/multi.hook.ts
@@ -1,3 +1,4 @@
+import { FlightBookingService } from "@/lib/services/flight/booking.service";
 import { ManyStaysRequestInput } from "./../../types/request-models/stay/search.type";
 import { StaySearchService } from "@/lib/services/stay/search.service";
 import { RateHawkLocationRequestInput } from "@/lib/types/request-models/stay/location.type";
@@ -5,45 +6,23 @@ import {
     SearchStayRequestRequestQuery,
     ViewSingleStayRequestInput,
 } from "@/lib/types/request-models/stay/search.type";
+import { SearchMultiFlightsResponse } from "@/lib/types/response-models/flight/multi_flight.type";
 import { RateHawkLocationSearchResponse } from "@/lib/types/response-models/stay/location.type";
 import {
     SearchStaysResponse,
     ViewSingleStayResponse,
 } from "@/lib/types/response-models/stay/search.type";
 import { UseQueryOptions, useQuery } from "@tanstack/react-query";
+import { SearchMultiFlightRequestQuery } from "@/lib/types/request-models/flight/booking.type";
 
-export const useSearchRateHawkLocations = (
-    params: RateHawkLocationRequestInput,
-    options?: UseQueryOptions<RateHawkLocationSearchResponse>
+export const useSearchMulticity = (
+    params: SearchMultiFlightRequestQuery,
+    options?: UseQueryOptions<SearchMultiFlightsResponse>
 ) => {
     return useQuery({
-        queryKey: ["ratehawk-locations", params.query],
-        queryFn: () => StaySearchService.searchRateHawkLocations(params),
-        ...options,
-    });
-};
-
-export const useSearchStays = (
-    params: {
-        query: SearchStayRequestRequestQuery;
-        payload: ManyStaysRequestInput;
-    },
-    options?: UseQueryOptions<SearchStaysResponse>
-) => {
-    return useQuery({
-        queryKey: ["search-ratehawk", params.query],
-        queryFn: () => StaySearchService.searchStays(params),
-        ...options,
-    });
-};
-
-export const useViewSingleStay = (
-    params: ViewSingleStayRequestInput,
-    options?: UseQueryOptions<ViewSingleStayResponse>
-) => {
-    return useQuery({
-        queryKey: ["view-single-stay", params.id],
-        queryFn: () => StaySearchService.viewSingleStay(params),
+        queryKey: ["search-multiflight", params],
+        queryFn: () =>
+            FlightBookingService.searchMultiFlights({ data: params }),
         ...options,
     });
 };
