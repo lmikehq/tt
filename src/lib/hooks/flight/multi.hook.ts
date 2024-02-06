006027c82be2b9627d8d71917f394a1313048fdb
Abdulazeez Olalere <olalereazeez11@gmail.com>
2024-02-06 18:37:08 +0100

    FEAT: fix issue with multicity sort

--- src/lib/hooks/flight/multi.hook.ts
+++ src/lib/hooks/flight/multi.hook.ts
@@ -65,7 +65,19 @@ export const useSearchMulticityBySort = (
     return useQueries({
         queries: [
             {
-                queryKey: ["search-multiflight-best-sort", params],
+                queryKey: [
+                    "search-multiflight-best-sort",
+                    {
+                        requests: [
+                            {
+                                ...params.requests[0],
+                                limit: 1,
+                                sort: FlightSortEnum.best,
+                            },
+                            ...params.requests.slice(1), // Keep the rest of the requests unchanged
+                        ],
+                    },
+                ],
                 queryFn: () => {
                     const query = {
                         requests: [
@@ -84,7 +96,19 @@ export const useSearchMulticityBySort = (
                 ...options,
             },
             {
-                queryKey: ["search-multiflight-cheapest-sort", params],
+                queryKey: [
+                    "search-multiflight-cheapest-sort",
+                    {
+                        requests: [
+                            {
+                                ...params.requests[0],
+                                limit: 1,
+                                sort: FlightSortEnum.cheapest,
+                            },
+                            ...params.requests.slice(1), // Keep the rest of the requests unchanged
+                        ],
+                    },
+                ],
                 queryFn: () => {
                     const query = {
                         requests: [
@@ -103,7 +127,19 @@ export const useSearchMulticityBySort = (
                 ...options,
             },
             {
-                queryKey: ["search-multiflight-fastest-sort", params],
+                queryKey: [
+                    "search-multiflight-fastest-sort",
+                    {
+                        requests: [
+                            {
+                                ...params.requests[0],
+                                limit: 1,
+                                sort: FlightSortEnum.fastest,
+                            },
+                            ...params.requests.slice(1), // Keep the rest of the requests unchanged
+                        ],
+                    },
+                ],
                 queryFn: () => {
                     const query = {
                         requests: [
@@ -122,7 +158,19 @@ export const useSearchMulticityBySort = (
                 ...options,
             },
             {
-                queryKey: ["search-multiflight-earliest-sort", params],
+                queryKey: [
+                    "search-multiflight-earliest-sort",
+                    {
+                        requests: [
+                            {
+                                ...params.requests[0],
+                                limit: 1,
+                                sort: FlightSortEnum.earliest,
+                            },
+                            ...params.requests.slice(1), // Keep the rest of the requests unchanged
+                        ],
+                    },
+                ],
                 queryFn: () => {
                     const query = {
                         requests: [
