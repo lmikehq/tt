e3ed5ed78d7e012e34653d5f5983c88ef327285d
David Bodunrin <bodunrindavidbond@gmail.com>
2024-02-12 22:25:27 +0100

    update types

--- src/lib/hooks/stay/index.hook.ts
+++ src/lib/hooks/stay/index.hook.ts
@@ -1,3 +1,26 @@
+// import { StayService } from "@/lib/services/stay/index.service";
+// import { LikeHotelRequestInput } from "@/lib/types/request-models/stay/index.type";
+// import { StaysLike, ViewSingleStayResponse } from "@/lib/types/response-models/stay/search.type";
+// import { UseMutationOptions, useMutation } from "@tanstack/react-query";
+
+// export const useLikeHotel = (
+//   options?: Omit<
+//     UseMutationOptions<
+//       StaysLike,
+//       unknown,
+//       LikeHotelRequestInput,
+//       unknown
+//     >,
+//     "mutationFn"
+//   >
+// ) => {
+//   return useMutation({
+//     mutationFn: (params: LikeHotelRequestInput): Promise<StaysLike> =>
+//       StayService.likeHotel(params),
+//     ...options,
+//   });
+// };
+
 import { StayService } from "@/lib/services/stay/index.service";
 import { LikeHotelRequestInput } from "@/lib/types/request-models/stay/index.type";
 import { LikeStayResponse } from "@/lib/types/response-models/stay/search.type";
