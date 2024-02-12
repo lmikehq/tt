e84e5f454fe0371b6f04b5de8abb69f760ddc658
David Bodunrin <bodunrindavidbond@gmail.com>
2024-02-12 13:45:46 +0100

    FEAT: ADD SUPPORT FOR OLD-VISA-APPLICATION

--- src/lib/extensions/helpers/type-guard.ts
+++ src/lib/extensions/helpers/type-guard.ts
@@ -2,7 +2,7 @@ import { VisaApplication } from "@/lib/types/visa";
 
 export function isVisaApplication(obj: any): obj is VisaApplication {
   return (
-    'personalDetails' in obj.primaryTraveller ||
+    'personalDetails' in obj?.primaryTraveller ||
     'familyInformation' in obj ||
     'guarantorInformation' in obj
   );
