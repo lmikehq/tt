07037c81df27b4776170ba6565545c1e209f84d6
Michael E <adebisienads@gmail.com>
2024-03-02 18:33:52 +0100

    blog section completed
    Signed-off-by: Michael E <adebisienads@gmail.com>

--- src/lib/hooks/blog/index.hook.ts
+++ src/lib/hooks/blog/index.hook.ts
@@ -1,6 +1,9 @@
 import { BlogService } from "@/lib/services/blog/index.service";
 import { FetchBlogsRequestInput } from "@/lib/types/request-models/blog/index.type";
-import { FetchBlogsResponse } from "@/lib/types/response-models/blog/index.type";
+import {
+    BlogInterface,
+    FetchBlogsResponse,
+} from "@/lib/types/response-models/blog/index.type";
 import { UseQueryOptions, useQuery } from "@tanstack/react-query";
 
 export const useFetchBlogs = (
@@ -10,6 +13,16 @@ export const useFetchBlogs = (
     return useQuery({
         queryKey: ["fetch-blogs", params],
         queryFn: () => BlogService.fetchBlogs(params),
+        ...options,
+    });
+};
+export const useFetchBlogBySlug = (
+    slug: string,
+    options?: UseQueryOptions<BlogInterface>
+) => {
+    return useQuery({
+        queryKey: ["fetch-blog-by-slug", slug],
+        queryFn: () => BlogService.blogBySlug(slug),
         ...options,
     });
 };
