0643005ac642ce40eb9d8522bcf6e64d34525c9b
Abdulazeez Olalere <olalereazeez11@gmail.com>
2024-03-13 15:35:44 +0100

    FEAT: fix bad image UI on dangerouslySetInnerHTML

--- src/components/molecules/countryArticle.tsx
+++ src/components/molecules/countryArticle.tsx
@@ -1,18 +1,18 @@
-
+import "../../styles/article.css";
 interface Props {
-  article: {
-    body: string;
-  };
+    article: {
+        body: string;
+    };
 }
 
 const CountryArticle = ({ article }: Props) => {
-  return (
-    <>
-      <article className="prose lg:prose-xl">
-        <div dangerouslySetInnerHTML={{ __html: article.body }} />
-      </article>
-    </>
-  );
+    return (
+        <>
+            <article id="blog" className="prose lg:prose-xl">
+                <div dangerouslySetInnerHTML={{ __html: article.body }} />
+            </article>
+        </>
+    );
 };
 
 export default CountryArticle;
