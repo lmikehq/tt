9821219c6c3f1b0fc9d4d2a7526949157480846e
BukunmiPeter <oluwabukunmiadeyemo99@gmail.com>
2024-03-28 13:06:24 +0100

    blog

--- src/lib/axios/axios-client.ts
+++ src/lib/axios/axios-client.ts
@@ -12,7 +12,7 @@ interface XAxiosInstance extends AxiosInstance {
   ): Promise<R>;
 }
 const axiosClient: AxiosInstance = axios.create({
-  baseURL: "http://localhost:5001",
+  baseURL:"http://localhost:5001",
   timeout: 15000,
   //   withCredentials: true,
   headers: {
