9b00c69ea222b684061a25e456fe101537187260
Abdulazeez Olalere <olalereazeez11@gmail.com>
2024-04-04 11:02:48 +0100

    FEAT: fix vercel error

--- src/app/blogs/[title]/layout.tsx
+++ src/app/blogs/[title]/layout.tsx
@@ -4,22 +4,11 @@ import { useParams } from "next/navigation";
 import { BlogService } from "@/lib/services/blog/index.service";
 import { Metadata, ResolvingMetadata } from "next";
 
-// export const metadata = {
-//     title: "Blog page review",
-//     description: "Thrillers Travels Blog Page",
-// };
-
-type Props = {
-    params: { title: string };
-    searchParams: { [key: string]: string | string[] | undefined };
-};
-export async function generateMetadata(
-    { params, searchParams }: Props,
-    parent: ResolvingMetadata
-): Promise<Metadata> {
-    console.log("paramis", params);
+export async function generateMetadata({
+    params,
+    searchParams,
+}: any): Promise<Metadata> {
     const blog = await BlogService.blogBySlug(params.title).then((res) => res);
-    console.log("paramis", blog);
 
     return {
         title: blog.metaDataTitle,
@@ -29,10 +18,6 @@ export async function generateMetadata(
             canonical: blog.canonicalTag,
         },
     };
-    // : {
-    //       title: "blog.metaDataTitle",
-    //       description: " blog.metaDataDesc",
-    //   };
 }
 
 export default function Layout({ children }: { children: React.ReactNode }) {
