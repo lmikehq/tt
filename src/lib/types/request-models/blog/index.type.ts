1031165eed023d9b450f9936a11914e50018a538
Abdulazeez Olalere <olalereazeez11@gmail.com>
2024-04-10 02:35:20 +0100

    FEAT: implement blog search functionality

--- src/lib/types/request-models/blog/index.type.ts
+++ src/lib/types/request-models/blog/index.type.ts
@@ -1,4 +1,11 @@
-export interface FetchBlogsRequestInput {}
+export interface FetchBlogsRequestInput {
+    status?: string;
+    title?: string;
+}
 export interface LikeBlogRequestInput {
     ip: string;
+}
+export enum BlogStatus {
+    PUBLISHED = "PUBLISHED",
+    DRAFT = "DRAFT",
 }
