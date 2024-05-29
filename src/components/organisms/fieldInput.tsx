a296266beafe184a098faa335e7de9c4f9289ebe
Abdulazeez Olalere <olalereazeez11@gmail.com>
2024-05-29 17:55:23 +0100

    FEAT: implement familyInfo vailation

--- src/components/organisms/fieldInput.tsx
+++ src/components/organisms/fieldInput.tsx
@@ -13,317 +13,312 @@ import InputLabel from "@mui/material/InputLabel";
 import PhoneInput from "react-phone-input-2";
 
 interface FieldProps {
-  id?: string;
-  value?: any;
-  defaultValue?: string;
-  styles?: CSSProperties;
-  padding?: string;
-  name: string;
-  type?:
-  | "text"
-  | "number"
-  | "file"
-  | "textArea"
-  | "password"
-  | "email"
-  | "tel"
-  | "address"
-  | "checkbox";
-  step?: string;
-  border?: string;
-  placeholder: string;
-  formik?: FormikValues;
-  options?: any[];
-  addon?: ReactNode;
-  views?: ("year" | "month" | "day")[];
-  disabled?: boolean;
-  onChange?: (x: any) => void;
-  onChanged?: (x?: any) => void;
-  minDate?: Dayjs | null;
-  maxDate?: Dayjs;
-  max?: number;
-  min?: number;
-  format?: string;
-  country?: string;
+    id?: string;
+    value?: any;
+    defaultValue?: string;
+    styles?: CSSProperties;
+    padding?: string;
+    name: string;
+    type?:
+        | "text"
+        | "number"
+        | "file"
+        | "textArea"
+        | "password"
+        | "email"
+        | "tel"
+        | "address"
+        | "checkbox";
+    step?: string;
+    border?: string;
+    placeholder: string;
+    formik?: FormikValues;
+    options?: any[];
+    addon?: ReactNode;
+    views?: ("year" | "month" | "day")[];
+    disabled?: boolean;
+    onChange?: (x: any) => void;
+    onChanged?: (x?: any) => void;
+    minDate?: Dayjs | null;
+    maxDate?: Dayjs;
+    max?: number;
+    min?: number;
+    format?: string;
+    country?: string;
 }
 
 function getNestedValue(obj: any, propertyPath: string) {
-  const properties = propertyPath.split(".");
-  let value = obj;
-  for (const prop of properties) {
-    if (value && typeof value === "object") {
-      value = value[prop];
-    } else {
-      return undefined;
+    const properties = propertyPath.split(".");
+    let value = obj;
+    for (const prop of properties) {
+        if (value && typeof value === "object") {
+            value = value[prop];
+        } else {
+            return undefined;
+        }
     }
-  }
-  return value;
+    return value;
 }
 
-export const ErrorText = ({ text }: { text: string; }) => {
-  return (
-    <Section styles={{ position: "relative", marginBottom: "1rem" }}>
-      <Section
-        height="fit-content"
-        styles={{ position: "absolute", top: "0.25rem" }}
-      >
-        <Text
-          type="p"
-          size={14}
-          weight={500}
-          color={ttColors.red}
-          styles={{ wordBreak: "break-all" }}
-          text={text}
-        />
-      </Section>
-    </Section>
-  );
+export const ErrorText = ({ text }: { text: string }) => {
+    return (
+        <Section styles={{ position: "relative", marginBottom: "1rem" }}>
+            <Section
+                height="fit-content"
+                styles={{ position: "absolute", top: "0.25rem" }}
+            >
+                <Text
+                    type="p"
+                    size={14}
+                    weight={500}
+                    color={ttColors.red}
+                    styles={{ wordBreak: "break-all" }}
+                    text={text}
+                />
+            </Section>
+        </Section>
+    );
 };
 
 export const FieldInput = (props: FieldProps) => {
-  const {
-    id,
-    name,
-    type,
-    placeholder,
-    formik,
-    addon,
-    onChange,
-    disabled,
-    min,
-    max,
-    border,
-  } = props;
+    const {
+        id,
+        name,
+        type,
+        placeholder,
+        formik,
+        addon,
+        onChange,
+        disabled,
+        min,
+        max,
+        border,
+    } = props;
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = e.target;
-    formik?.setFieldValue(name, value);
-  };
-  const touched = getNestedValue(formik?.touched, name);
-  const error = getNestedValue(formik?.errors, name);
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { value } = e.target;
+        formik?.setFieldValue(name, value);
+    };
+    const touched =
+        getNestedValue(formik?.touched, name) || formik?.touched[name];
+    const error = getNestedValue(formik?.errors, name);
 
-  return (
-    <Section>
-      <InputLabel />
-      <Input
-        id={id}
-        height="45px"
-        border={border}
-        addon={addon}
-        readOnly={disabled}
-        min={min}
-        max={max}
-        type={type}
-        placeholder={placeholder}
-        padding="0 0 0 14px"
-        onChange={onChange ? onChange : handleChange}
-        value={getNestedValue(formik?.values, name)}
-        onBlur={() =>
-          formik?.setTouched({ ...formik.touched, [name]: true })
-        }
-        error={touched && error}
-      />
-      {touched && error ? <ErrorText text={error} /> : null}
-    </Section>
-  );
+    return (
+        <Section>
+            <InputLabel />
+            <Input
+                id={id}
+                height="45px"
+                border={border}
+                addon={addon}
+                readOnly={disabled}
+                min={min}
+                max={max}
+                type={type}
+                placeholder={placeholder}
+                padding="0 0 0 14px"
+                onChange={onChange ? onChange : handleChange}
+                value={getNestedValue(formik?.values, name)}
+                onBlur={() =>
+                    formik?.setTouched({ ...formik.touched, [name]: true })
+                }
+                error={touched && error}
+            />
+            {touched && error ? <ErrorText text={error} /> : null}
+        </Section>
+    );
 };
 
 export const ArrayInput = (props: FieldProps) => {
-  const {
-    name,
-    type,
-    placeholder,
-    formik,
-    addon,
-    max,
-    min,
-    defaultValue,
-    step,
-  } = props;
+    const {
+        name,
+        type,
+        placeholder,
+        formik,
+        addon,
+        max,
+        min,
+        defaultValue,
+        step,
+    } = props;
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    let { value } = e.target;
-    if (name.includes("cgpa")) {
-      parseFloat(value) > 5 ? (value = "5") : value;
-    }
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        let { value } = e.target;
+        if (name.includes("cgpa")) {
+            parseFloat(value) > 5 ? (value = "5") : value;
+        }
 
-    formik?.setFieldValue(name, value);
-  };
+        formik?.setFieldValue(name, value);
+    };
 
-  const value = getNestedValue(formik?.values, name);
-  const touched = getNestedValue(formik?.touched, name);
-  const error = getNestedValue(formik?.errors, name);
+    const value = getNestedValue(formik?.values, name);
+    const touched = getNestedValue(formik?.touched, name);
+    const error = getNestedValue(formik?.errors, name);
 
-  return (
-    <div>
-      <Input
-        height="45px"
-        addon={addon}
-        step={step}
-        type={type}
-        max={max}
-        min={min}
-        defaultValue={defaultValue}
-        placeholder={placeholder}
-        padding="0 0 0 14px"
-        onChange={handleChange}
-        value={value == 0 ? "" : value}
-        onBlur={formik?.handleBlur}
-        error={touched && error}
-      />
-      {touched && error ? <ErrorText text={error} /> : null}
-    </div>
-  );
+    return (
+        <div>
+            <Input
+                height="45px"
+                addon={addon}
+                step={step}
+                type={type}
+                max={max}
+                min={min}
+                defaultValue={defaultValue}
+                placeholder={placeholder}
+                padding="0 0 0 14px"
+                onChange={handleChange}
+                value={value == 0 ? "" : value}
+                onBlur={formik?.handleBlur}
+                error={touched && error}
+            />
+            {touched && error ? <ErrorText text={error} /> : null}
+        </div>
+    );
 };
 
 export const FieldAsString = (props: FieldProps) => {
-  const {
-    name,
-    options = [],
-    formik,
-    placeholder,
-    value,
-    onChange,
-    disabled,
-  } = props;
-  const touched = getNestedValue(formik?.touched, name);
-  const error = getNestedValue(formik?.errors, name);
+    const {
+        name,
+        options = [],
+        formik,
+        placeholder,
+        value,
+        onChange,
+        disabled,
+    } = props;
+    const touched = getNestedValue(formik?.touched, name);
+    const error = getNestedValue(formik?.errors, name);
 
-  const handleChange = (e: any) => {
-    formik?.setFieldValue(name, e);
-  };
+    const handleChange = (e: any) => {
+        formik?.setFieldValue(name, e);
+    };
 
-  const formikvalue = getNestedValue(formik?.values, name);
+    const formikvalue = getNestedValue(formik?.values, name);
 
-  return (
-    <Section styles={{ position: "relative" }}>
-      <SearchFlagInput
-        value={value ? value : formikvalue}
-        options={options}
-        onChange={onChange ? onChange : handleChange}
-        placeholder={placeholder}
-        disabled={disabled}
-        error={touched && error}
-      />{" "}
-      {touched && error ? <ErrorText text={error.name} /> : null}
-    </Section>
-  );
+    return (
+        <Section styles={{ position: "relative" }}>
+            <SearchFlagInput
+                value={value ? value : formikvalue}
+                options={options}
+                onChange={onChange ? onChange : handleChange}
+                placeholder={placeholder}
+                disabled={disabled}
+                error={touched && error}
+            />{" "}
+            {touched && error ? <ErrorText text={error.name} /> : null}
+        </Section>
+    );
 };
 
 export const FieldString = (props: FieldProps) => {
-  const {
-    name,
-    options = [],
-    formik,
-    placeholder,
-    onChange,
-    value,
-    onChanged,
-  } = props;
-  const touched = getNestedValue(formik?.touched, name);
-  const error = getNestedValue(formik?.errors, name);
+    const {
+        name,
+        options = [],
+        formik,
+        placeholder,
+        onChange,
+        value,
+        onChanged,
+    } = props;
+    const touched = getNestedValue(formik?.touched, name);
+    const error = getNestedValue(formik?.errors, name);
 
-  const handleChange = (e: any) => {
-    // console.log("ee: ", e);
-    onChanged && onChanged(e);
-    formik?.setFieldValue(name, e);
-  };
+    const handleChange = (e: any) => {
+        // console.log("ee: ", e);
+        onChanged && onChanged(e);
+        formik?.setFieldValue(name, e);
+    };
 
-  const formikvalue = getNestedValue(formik?.values, name);
+    const formikvalue = getNestedValue(formik?.values, name);
 
-  return (
-    <Section styles={{ position: "relative" }}>
-      <SearchStringInput
-        options={options}
-        onChange={onChange ? onChange : handleChange}
-        placeholder={placeholder}
-        value={value ? value : formikvalue}
-        error={touched && error}
-      />
-      {touched && error ? <ErrorText text={error} /> : null}
-    </Section>
-  );
+    return (
+        <Section styles={{ position: "relative" }}>
+            <SearchStringInput
+                options={options}
+                onChange={onChange ? onChange : handleChange}
+                placeholder={placeholder}
+                value={value ? value : formikvalue}
+                error={touched && error}
+            />
+            {touched && error ? <ErrorText text={error} /> : null}
+        </Section>
+    );
 };
 
 export const FieldAsDate = (props: FieldProps) => {
-  const {
-    name,
-    disabled,
-    formik,
-    views,
-    onChange,
-    minDate,
-    maxDate,
-    placeholder,
-    format,
-    styles,
-    padding
-  } = props;
-  const touched = getNestedValue(formik?.touched, name);
-  const error = getNestedValue(formik?.errors, name);
+    const {
+        name,
+        disabled,
+        formik,
+        views,
+        onChange,
+        minDate,
+        maxDate,
+        placeholder,
+        format,
+        styles,
+        padding,
+    } = props;
+    const touched = getNestedValue(formik?.touched, name);
+    const error = getNestedValue(formik?.errors, name);
 
-  const handleChange = (e: any) => {
-    formik?.setFieldValue(name, `${dayjs(e, 'MM/DD/YYYY').format(format)}`);
-    // console.log('changing', dayjs(e, format).format(format))
-  };
+    const handleChange = (e: any) => {
+        formik?.setFieldValue(name, `${dayjs(e, "MM/DD/YYYY").format(format)}`);
+        // console.log('changing', dayjs(e, format).format(format))
+    };
 
-  const value = getNestedValue(formik?.values, name);
+    const value = getNestedValue(formik?.values, name);
 
-  return (
-    <Section
-      styles={{ position: "relative", ...styles }}
-      padding={padding || "0 0 1.2rem 0"}
-    >
-      <DatePicker
-        disabled={disabled}
-        views={views}
-        placeholder={placeholder}
-        maxDate={maxDate}
-        minDate={minDate}
-        value={dayjs(`${value}`, format)}
-        onChange={onChange ? onChange : handleChange}
-        error={touched && error}
-        format={format}
-      />
-      {touched && error ? <ErrorText text={error} /> : null}
-    </Section>
-  );
+    return (
+        <Section
+            styles={{ position: "relative", ...styles }}
+            padding={padding || "0 0 1.2rem 0"}
+        >
+            <DatePicker
+                disabled={disabled}
+                views={views}
+                placeholder={placeholder}
+                maxDate={maxDate}
+                minDate={minDate}
+                value={dayjs(`${value}`, format)}
+                onChange={onChange ? onChange : handleChange}
+                error={touched && error}
+                format={format}
+            />
+            {touched && error ? <ErrorText text={error} /> : null}
+        </Section>
+    );
 };
 
-
 export const FieldPhone = (props: FieldProps) => {
-  const {
-    name,
-    formik,
-    placeholder,
-    onChange,
-    value,
-    country,
-    onChanged,
-  } = props;
-  const touched = getNestedValue(formik?.touched, name);
-  const error = getNestedValue(formik?.errors, name);
+    const { name, formik, placeholder, onChange, value, country, onChanged } =
+        props;
+    const touched = getNestedValue(formik?.touched, name);
+    const error = getNestedValue(formik?.errors, name);
 
-  const handleChange = (e: any) => {
-    onChanged && onChanged(e);
-    formik?.setFieldValue(name, e);
-  };
+    const handleChange = (e: any) => {
+        onChanged && onChanged(e);
+        formik?.setFieldValue(name, e);
+    };
 
-  const formikvalue = getNestedValue(formik?.values, name);
+    const formikvalue = getNestedValue(formik?.values, name);
 
-  return (
-    <Section styles={{ position: "relative" }}>
-      <PhoneInput
-        country={country ?? "ng"}
-        autoFormat={true}
-        value={formikvalue}
-        inputProps={{ name }}
-        inputStyle={{ border: touched && error ? `1px solid crimson` : '' }}
-        onChange={handleChange}
-        inputClass="w"
-        placeholder={placeholder ?? "Enter phone number"}
-      />
-      {touched && error ? <ErrorText text={error} /> : null}
-    </Section>
-  );
+    return (
+        <Section styles={{ position: "relative" }}>
+            <PhoneInput
+                country={country ?? "ng"}
+                autoFormat={true}
+                value={formikvalue}
+                inputProps={{ name }}
+                inputStyle={{
+                    border: touched && error ? `1px solid crimson` : "",
+                }}
+                onChange={handleChange}
+                inputClass="w"
+                placeholder={placeholder ?? "Enter phone number"}
+            />
+            {touched && error ? <ErrorText text={error} /> : null}
+        </Section>
+    );
 };
--- src/components/organisms/form/components/employmentInfo.tsx
+++ src/components/organisms/form/components/employmentInfo.tsx
@@ -1,11 +1,11 @@
 import Section from "src/components/molecules/section";
 import {
-  FieldArray,
-  Formik,
-  FormikProps,
-  FormikProvider,
-  FormikValues,
-  useFormik,
+    FieldArray,
+    Formik,
+    FormikProps,
+    FormikProvider,
+    FormikValues,
+    useFormik,
 } from "formik";
 import FormStepTitle from "./formStepsTitle";
 import { useScreenResolution } from "@lib/extensions/hook/useScreenResolution";
@@ -16,9 +16,9 @@ import { RiDeleteBin6Line } from "react-icons/ri";
 import Text from "@atom/text";
 import AddButton from "@molecule/addButton";
 import {
-  employmentKeys,
-  employmentsArr,
-  manyEmploymentSchema,
+    employmentKeys,
+    employmentsArr,
+    manyEmploymentSchema,
 } from "@lib/types/schema";
 import ContinueButton from "@organism/continueButton";
 import { EmploymentDetailsInterface, Mode } from "@lib/types";
@@ -27,82 +27,114 @@ import { useApplicationFormStore } from "@lib/store/application-form.store";
 import ToastError from "@molecule/toastError";
 
 interface formProps {
-  steps: string[];
-  index: number;
-  persistForm: () => void;
-  formik: FormikProps<{ employment: EmploymentDetailsInterface[] }>;
+    steps: string[];
+    index: number;
+    persistForm: () => void;
+    formik: FormikProps<{ employment: EmploymentDetailsInterface[] }>;
 }
 
 function EmploymentInfo({ steps, index, persistForm, formik }: formProps) {
-  const { mode } = useApplicationFormStore((state) => state);
-  const isLoading = mode == Mode.loading;
+    const { mode } = useApplicationFormStore((state) => state);
+    const isLoading = mode == Mode.loading;
 
-  return (
-    <FormikProvider value={formik}>
-      <Section>
-        <form onSubmit={formik.handleSubmit}>
-          <FieldArray
-            name="employment"
-            render={(arrayHelpers) => (
-              <div>
-                <Flex justify="space-between" padding="0 0 2rem 0" align="flex-start">
-                  <FormStepTitle steps={steps} index={index} />
-                  <AddButton
-                    disabled={formik.values.employment.length === 5}
-                    onClick={() => {
-                      if (!formik.isValid)
-                        return toast.error("Please validate all inputs");
-                      if (formik.values.employment.length < 5) {
-                        arrayHelpers.insert(index + 1, employmentKeys);
-                      }
-                    }}
-                  />
-                </Flex>
-                {formik.values.employment.map((employment, index, arr) => (
-                  <div key={index} style={{ marginBottom: '3.5rem' }}>
-                    <EmploymentForm
-                      formik={formik}
-                      values={employment}
-                        count={index}
-                        length={arr.length}
+    return (
+        <FormikProvider value={formik}>
+            <Section>
+                <form onSubmit={formik.handleSubmit}>
+                    <FieldArray
+                        name="employment"
+                        render={(arrayHelpers) => (
+                            <div>
+                                <Flex
+                                    justify="space-between"
+                                    padding="0 0 2rem 0"
+                                    align="flex-start"
+                                >
+                                    <FormStepTitle
+                                        steps={steps}
+                                        index={index}
+                                    />
+                                    <AddButton
+                                        disabled={
+                                            formik.values.employment.length ===
+                                            5
+                                        }
+                                        onClick={() => {
+                                            if (!formik.isValid)
+                                                return toast.error(
+                                                    "Please validate all inputs"
+                                                );
+                                            if (
+                                                formik.values.employment
+                                                    .length < 5
+                                            ) {
+                                                arrayHelpers.insert(
+                                                    index + 1,
+                                                    employmentKeys
+                                                );
+                                            }
+                                        }}
+                                    />
+                                </Flex>
+                                {formik.values.employment.map(
+                                    (employment, index, arr) => (
+                                        <div
+                                            key={index}
+                                            style={{ marginBottom: "3.5rem" }}
+                                        >
+                                            <EmploymentForm
+                                                formik={formik}
+                                                values={employment}
+                                                count={index}
+                                                length={arr.length}
+                                            />
+                                            {formik.values.employment.length >
+                                                1 && (
+                                                <Flex
+                                                    justify="flex-end"
+                                                    gap="0.25rem"
+                                                    align="center"
+                                                    onClick={() =>
+                                                        arrayHelpers.remove(
+                                                            index
+                                                        )
+                                                    }
+                                                    cursor="pointer"
+                                                >
+                                                    <RiDeleteBin6Line
+                                                        color={ttColors.red}
+                                                        size={25}
+                                                    />
+                                                    <Text
+                                                        type="p"
+                                                        text="Delete Experience"
+                                                        color={ttColors.red}
+                                                        weight="500"
+                                                        size={15}
+                                                    />
+                                                </Flex>
+                                            )}
+                                        </div>
+                                    )
+                                )}
+                            </div>
+                        )}
                     />
-                    {formik.values.employment.length > 1 && (
-                      <Flex
-                        justify="flex-end"
-                        gap="0.25rem"
-                        align="center"
-                        onClick={() => arrayHelpers.remove(index)}
-                        cursor="pointer"
-                      >
-                        <RiDeleteBin6Line color={ttColors.red} size={25} />
-                        <Text
-                          type="p"
-                          text="Delete Experience"
-                          color={ttColors.red}
-                            weight="500"
-                            size={15}
-                        />
-                      </Flex>
-                    )}
-                  </div>
-                ))}
-              </div>
-            )}
-          />
-            <ContinueButton
-                isLoading={isLoading}
-                saveProgressAndContinueLater={persistForm}
-                onClick={() => {
-                    formik.validateForm().then(res => {
-                        if (Object.keys(res).length > 0) return ToastError();
-                    })
-                }}
-                disabled={!formik.isValid}
-          />
-        </form>
-      </Section>
-    </FormikProvider>
-  );
+                    <ContinueButton
+                        isLoading={isLoading}
+                        saveProgressAndContinueLater={persistForm}
+                        onClick={() => {
+                            formik.validateForm().then((res) => {
+                                if (Object.keys(res).length > 0)
+                                    return ToastError();
+                            });
+                        }}
+                        disabled={!formik.isValid}
+                    />
+                </form>
+            </Section>
+        </FormikProvider>
+    );
 }
 
 export default EmploymentInfo;
